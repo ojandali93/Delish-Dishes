33d578ff23817e82d568c9cce5e6ff9a52525b15
omar jandali <omarjandali93@gmail.com>
2024-10-28 00:51:57 -0700

    Omar Jandali. Converted Dine WIth Me to Delish Dishes

--- android/app/src/main/java/com/delishdishes/MainActivity.kt
+++ android/app/src/main/java/com/delishdishes/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.delishdishes
+package com.delish dishes
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "DelishDishes"
+  override fun getMainComponentName(): String = "Delish Dishes"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/delishdishes/MainApplication.kt
+++ android/app/src/main/java/com/delishdishes/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.delishdishes
+package com.delish dishes
 
 import android.app.Application
 import com.facebook.react.PackageList
